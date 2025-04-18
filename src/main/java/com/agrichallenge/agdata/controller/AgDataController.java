6b4e24f6900ba16f06803dd5553cd5c82d60c7c7
sachin634 <78214246+sachin634@users.noreply.github.com>
2025-04-19 00:34:55 +0530

    Update AgDataController.java

--- src/main/java/com/agrichallenge/agdata/controller/AgDataController.java
+++ src/main/java/com/agrichallenge/agdata/controller/AgDataController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,13 +30,46 @@ public class AgDataController {
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
-    // TODO: GET /api/agdata/crop-count?cropName=corn
+    @GetMapping("/crop-count")
+    public ResponseEntity<Long> getAllCropName(@RequestParam("cropName") String name){
+        Long data=agDataService.getCropCount(name);
+        if(data==null){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+
+        }
+
+        return new ResponseEntity<>(data, HttpStatus.OK);
+
+    }
+
 
 
     // TODO: GET /api/agdata/average-yield?cropName=wheat
 
+    @GetMapping("/average-yield")
+    public ResponseEntity<Double> getAverageYielEntity(@RequestParam("cropName") String name){
+        Double data=agDataService.getAverageYield(name);
+        if(data==null){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(data, HttpStatus.OK);
+
+    }
 
     // TODO: GET /api/agdata/by-region?region=Midwest
+
+    @GetMapping("by-region")
+    public ResponseEntity<List<AgData>> getRegionData(@RequestParam("region") String name){
+        List<AgData> list=agDataService.getRecordsByRegion(name);
+        if(list==null || list.isEmpty() || list.size()==0){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
+
+    }
+
 
 
 }
