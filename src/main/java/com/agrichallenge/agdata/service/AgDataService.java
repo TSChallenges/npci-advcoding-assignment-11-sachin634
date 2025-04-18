0fb4234b92733d797b5683508fe8b932ebc4e154
sachin634 <78214246+sachin634@users.noreply.github.com>
2025-04-19 00:34:19 +0530

    Update AgDataService.java

--- src/main/java/com/agrichallenge/agdata/service/AgDataService.java
+++ src/main/java/com/agrichallenge/agdata/service/AgDataService.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,20 +33,36 @@ public class AgDataService {
 
     public Long getCropCount(String cropName) {
         // TODO: Implement this method to Count how many times a specific crop appears in the dataset
-
-        return 0L;
+        Long count =0L;
+        for(AgData agData : agDataList){
+            if(agData.getCrop().equalsIgnoreCase(cropName))
+                count++;
+        }
+        return count;
     }
 
     public double getAverageYield(String cropName) {
         // TODO: Implement this method to Calculate the average yield for a specific crop if it exists, else return 0.0
-
-        return 0.0;
+        int count =0;
+        double totalYield = 0.0;
+        for(AgData agData:agDataList){
+            if(agData.getCrop().equalsIgnoreCase(cropName)){
+                count++;
+                totalYield+=agData.getYield();
+            }
+        }
+        return totalYield/count;
     }
 
     public List<AgData> getRecordsByRegion(String region) {
         // TODO: Implement this method to Get all records from a specific region
-
-        return null;
+        List<AgData> data=new ArrayList();
+        for(AgData agData:agDataList){
+            if(agData.getRegion().equalsIgnoreCase(region)){
+                data.add(agData);
+            }
+        }
+        return data;
     }
 
 }
