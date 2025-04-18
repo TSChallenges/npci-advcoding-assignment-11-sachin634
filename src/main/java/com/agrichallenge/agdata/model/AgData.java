148d9ef163ef574313eb28e486547a85df673b77
sachin634 <78214246+sachin634@users.noreply.github.com>
2025-04-19 00:36:07 +0530

    Update AgData.java

--- src/main/java/com/agrichallenge/agdata/model/AgData.java
+++ src/main/java/com/agrichallenge/agdata/model/AgData.java
@@ -8,9 +8,59 @@ public class AgData {
     private Integer yield;
     private Integer year;
 
+    public AgData(){
+
+    }
+
+    public AgData(int id,String crop,String region,int yield,int year){
+        this.id = id;
+        this.crop = crop;
+        this.region = region;
+        this.yield=yield;
+        this.year = year;
+    }
+
 
     // TODO: Default constructor - No arguments
+    public void setId(int id){
+        this.id = id;
+    }
 
+    public int getId(){
+        return id;
+    }
+
+    public void setCrop(String crop){
+        this.crop = crop;
+    }
+
+    public String getCrop(){
+        return crop;
+    }
+
+    public String getRegion(){
+        return region;
+    }
+
+    public void setRegion(String region){
+        this.region = region;
+    }
+
+    public int getYield(){
+        return yield;
+    }
+
+    public void setYield(int yield){
+        this.yield = yield;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
 
     // TODO: Parameterized constructor
 
